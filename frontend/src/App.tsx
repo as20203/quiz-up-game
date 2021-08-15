2ad9fc0f135a70a74aef51be1613726578434554
as20203 <jzaheer.bese16seecs@seecs.edu.pk>
2021-08-15 17:22:58 +0500

    Adds material ui to project.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Typography } from '@material-ui/core';
+const App = () => {
+  return <Typography variant='h4'>Hello World</Typography>;
+};
 
 export default App;
